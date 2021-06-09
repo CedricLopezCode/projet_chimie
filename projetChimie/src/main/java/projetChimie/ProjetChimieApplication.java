cbdc90ae2be4136e12f779f6b353e470034872cc
Cedric <cedric-lopez@hotmail.fr>
2021-06-09 10:51:48 +0200

    creation projet

---
+++ projetChimie/src/main/java/projetChimie/ProjetChimieApplication.java
@@ -0,0 +1,13 @@
+package projetChimie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetChimieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetChimieApplication.class, args);
+	}
+
+}
---
+++ projetChimie/src/test/java/projetChimie/ProjetChimieApplicationTests.java
@@ -0,0 +1,13 @@
+package projetChimie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetChimieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
