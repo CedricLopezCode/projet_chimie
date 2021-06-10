29d153d9dea4bb85a4672f8086b94264c9f5cc80
Cedric <cedric-lopez@hotmail.fr>
2021-06-10 10:07:29 +0200

    page d'accueil

--- projetChimie/src/main/java/projetChimie/model/Molecule.java
+++ projetChimie/src/main/java/projetChimie/model/Molecule.java
@@ -31,6 +31,24 @@ public class Molecule {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Molecule(int nbC, int nbH, int nbO, int nbN, boolean alcene, int numC_alcene, boolean alcyne, int numC_alcyne, boolean alcool, int numC_alcool, boolean amine, int numC_amine, float masseMolaire) {
+		super();
+		this.nbC = nbC;
+		this.nbH = nbH;
+		this.nbO = nbO;
+		this.nbN = nbN;
+		this.alcene = alcene;
+		this.numC_alcene = numC_alcene;
+		this.alcyne = alcyne;
+		this.numC_alcyne = numC_alcyne;
+		this.alcool = alcool;
+		this.numC_alcool = numC_alcool;
+		this.amine = amine;
+		this.numC_amine = numC_amine;
+		this.masseMolaire = masseMolaire;
+	}
+	
+
 	
 	
 	
--- projetChimieAngular/src/app/app-routing.module.ts
+++ projetChimieAngular/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ElementComponent } from './page/element/element.component';
+import { HomeComponent } from './page/home/home.component';
 import { MoleculeComponent } from './page/molecule/molecule.component';
 import { RetrosyntheseComponent } from './page/retrosynthese/retrosynthese.component';
 
 const routes: Routes = [
-  { path: 'pageElement', component: ElementComponent },
-  { path: 'pageMolecule', component: MoleculeComponent },
-  { path: 'pageRetrosynthese', component: RetrosyntheseComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'atomes', component: ElementComponent },
+  { path: 'molecule', component: MoleculeComponent },
+  { path: 'retrosynthese', component: RetrosyntheseComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
